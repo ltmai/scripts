ae189698d13fcab361b0699449b83c0780fa91a1
ltmai <linhtmai@yahoo.com>
2018-10-22 14:08:03 +0200

    Simplified example

--- oralce/generate_insert_scripts/generateInsertScripts.sql
+++ oralce/generate_insert_scripts/generateInsertScripts.sql
@@ -54,7 +54,7 @@ DECLARE
             END LOOP;
 
             RETURN ret;
-        END;
+        END getFieldList;
 
 
         /*
@@ -78,7 +78,7 @@ DECLARE
             END LOOP;
 
             RETURN ret;
-        END;
+        END getPkColumns;
 
         /*
         ** returns column value as text depending on column data type
@@ -111,7 +111,7 @@ DECLARE
             END IF;
 
             RETURN ret;
-        END;
+        END getColumnValue;
 
         /*
         ** returns SQL code to NULL-check and write corresponding column values
@@ -140,7 +140,7 @@ DECLARE
             END LOOP;
 
             RETURN ret;
-        END;
+        END getValueList;
 
     BEGIN -- createInsertScript
 
@@ -176,214 +176,19 @@ DECLARE
         --DBMS_OUTPUT.PUT_LINE(sqlCmd);
         EXECUTE IMMEDIATE sqlCmd;
 
-    END; -- createInsertScript
+    END createInsertScript; 
 
 BEGIN
+
     DBMS_OUTPUT.PUT_LINE('BEGIN');
 
-    --------- testing with one table----------
-    IF FALSE THEN
-        createInsertScript('SYS_CONSTANT');
-        RETURN;
-    END IF;
-    --------- testing with one table----------
-
-    --createInsertScript('SYS_APPSESSION');
-    createInsertScript('SYS_CONSTANT');
-    createInsertScript('SYS_DBEREG');
-    --createInsertScript('SYS_EVENT');
-    createInsertScript('SYS_LANGUAGE');
-    createInsertScript('SYS_LOCALE');
-    createInsertScript('SYS_MODULE');
-    createInsertScript('SYS_PARAMETER');
-    --createInsertScript('SYS_PICTURE'); -- SYS_PICTURE.PICTURE is of type BLOB
-    createInsertScript('SYS_STATE');
-    createInsertScript('SYS_TEXTBLOCK');
-    createInsertScript('SYS_TB_TEXT');
-    --createInsertScript('SYS_TRANSACTION');
-    createInsertScript('SYS_WTM');
-    createInsertScript('SYS_WTM_SHIFT');
-    createInsertScript('SYS_WTM_WEEK');
-    createInsertScript('SYS_WEEK_CAL_TEMPL');
-    createInsertScript('SYS_WEEK_CAL_SHIFT');
-
-    createInsertScript('KNL_ACTION');
-    --createInsertScript('KNL_ACTIVITY'); -- created by PL/SQL
-    --createInsertScript('KNL_ACTIVITY_DETAIL');
-    createInsertScript('KNL_CONTROL');
-    --createInsertScript('KNL_EVENTLOG');
-    createInsertScript('KNL_FLOW');
-    createInsertScript('KNL_FLOW_DETAIL');
-    createInsertScript('KNL_JOB');
-    --createInsertScript('KNL_MANUALTELEGRAM');
-    createInsertScript('KNL_MSQ');
-    createInsertScript('KNL_PARAM');
-    createInsertScript('KNL_PARAMPROC');
-    createInsertScript('KNL_SYNC');
-    createInsertScript('KNL_SYNC_DETAIL');
-    --createInsertScript('KNL_TRACE');
-    --createInsertScript('KNL_TRACEOFSCHEDULED');
-
-    --createInsertScript('EGW_ACTIVITY');
-    createInsertScript('EGW_CHANNEL');
-    createInsertScript('EGW_CHANNELSTATE');
-    createInsertScript('EGW_ERRORCODE');
-    createInsertScript('EGW_MSGDIRECT');
-    createInsertScript('EGW_MSGSTATE');
-    --createInsertScript('EGW_PLCMSG');
-    createInsertScript('EGW_TMERROR');
-    --createInsertScript('EGW_XMLMSG');
-
-    --createInsertScript('MFC_DATAMODULE'); -- MFC_DATAMODULE.DATAMODULE is of type BLOB
-    createInsertScript('MFC_DESTINATION2REDIRECT');
-    createInsertScript('MFC_HEIGHTCLS');
-    createInsertScript('MFC_KEYPRESSED');
-    --createInsertScript('MFC_LANE');
-    --createInsertScript('MFC_LANE_DEST');
-    --createInsertScript('MFC_LANESTATE');
-    --createInsertScript('MFC_OPERATIONTIME');
-    createInsertScript('MFC_PATH');
-    --createInsertScript('MFC_PCHECK');
-    --createInsertScript('MFC_PCHECK_DETAIL');
-    createInsertScript('MFC_PCHECK2REDIRECT');
-    --createInsertScript('MFC_PDATA');
-    --createInsertScript('MFC_PDATA_DETAIL');
-    createInsertScript('MFC_PFC');
-    createInsertScript('MFC_POINT');
-    createInsertScript('MFC_POINT2FLOW');
-    createInsertScript('MFC_POINT2FTPK');
-    createInsertScript('MFC_POINT2TO');
-    createInsertScript('MFC_RECONCILIATIONCONFIG');
-    createInsertScript('MFC_ROUTE');
-    createInsertScript('MFC_RSCGRP');
-    createInsertScript('MFC_SYNC_TO');
-    --createInsertScript('MFC_TEST_ROUTING');
-    --createInsertScript('MFC_TEST_VSW');
-    --createInsertScript('MFC_THROUGHPUT');
-    createInsertScript('MFC_TM');
-    createInsertScript('MFC_TMDEVICE');
-    --createInsertScript('MFC_TO');
-    --createInsertScript('MFC_TOBINCOORD');
-    createInsertScript('MFC_TOPARAM');
-    createInsertScript('MFC_TUTYPE');
-    createInsertScript('MFC_TUTYPECLASS');
-    createInsertScript('MFC_TUTYPE2TM');
-
-    createInsertScript('MMD_ARTCLS');
-    createInsertScript('MMD_ARTCLSPROP');
-    createInsertScript('MMD_ARTCLSSTATE');
-    createInsertScript('MMD_ARTICLE');
-    createInsertScript('MMD_ARTPROP');
-    createInsertScript('MMD_ARTSTATE');
-    createInsertScript('MMD_ARTUNIT');
-    createInsertScript('MMD_ARTVAR');
-    createInsertScript('MMD_CLIENT');
-    createInsertScript('MMD_CLIENTACCESS');
-    createInsertScript('MMD_CVAL');
-    createInsertScript('MMD_CVALLVL');
-    createInsertScript('MMD_CVALSEQ');
-    createInsertScript('MMD_CVALSEQELEMENT');
-    createInsertScript('MMD_HUCLS');
-    createInsertScript('MMD_HUCLSARTLIMIT');
-    createInsertScript('MMD_HUCLSNEST');
-    createInsertScript('MMD_HUCLSPROP');
-    createInsertScript('MMD_PICKINGLVL');
-    createInsertScript('MMD_PROP');
-    createInsertScript('MMD_PROPHULVL');
-    createInsertScript('MMD_PROPVALUE');
-    createInsertScript('MMD_REFENTITY');
-    createInsertScript('MMD_UNIT');
-    createInsertScript('MMD_VARIANT');
-    createInsertScript('MMD_VARIANTPROP');
-
-    createInsertScript('INV_ALLOCTYPE');
-    --createInsertScript('INV_HU');
-    --createInsertScript('INV_HUJNL');
-    createInsertScript('INV_HULOCK');
-    createInsertScript('INV_HUPROP');
-    createInsertScript('INV_HUPROPJNL');
-    --createInsertScript('INV_LOCALLOC');
-    createInsertScript('INV_STRATEGYMAP');
-    --createInsertScript('INV_SUMMARY');
-
-    createInsertScript('CRM_TMDEVICE');
-    createInsertScript('CRM_FORK');
-    createInsertScript('CRM_POINT');
-    createInsertScript('CRM_POINT2FTPK');
-    createInsertScript('CRM_TMERROR2PROC');
-    --createInsertScript('CRM_TO');
-    --createInsertScript('DEV_TRC_BUFFER'); -- no primary key found
-
-    createInsertScript('OMS_CANCEL');
-    createInsertScript('OMS_EXECCODE');
-    createInsertScript('OMS_ORDER');
-    createInsertScript('OMS_ORDHU');
-    createInsertScript('OMS_ORDSCHED');
-    createInsertScript('OMS_ORDSTATE');
-    createInsertScript('OMS_ORDTYPE');
-    createInsertScript('OMS_PROGRAM');
-    createInsertScript('OMS_REQEXECCODE');
-    createInsertScript('OMS_RESET');
-    createInsertScript('OMS_STATEMACHINE');
-    createInsertScript('OMS_STATETRANS');
-    createInsertScript('OMS_SUBTYPE');
-    createInsertScript('OMS_TEMPORDER');
-
-    createInsertScript('MOM_MFCSTRATEGYMAP');
-    createInsertScript('MOM_RELOCDEST');
-    createInsertScript('MOM_TOTYPEMAP');
-    createInsertScript('MOM_TUTYPEMAP');
-    createInsertScript('MRM_POINT');
-    createInsertScript('MRM_SIMULATION');
-    createInsertScript('MRM_TORECONCILIATION');
-
-    createInsertScript('PCS_TELE_CONFIGURATION');
-    createInsertScript('PLC_CFG_PARAM');
-
-    createInsertScript('PRJ_PDATA');
-    createInsertScript('PRJ_PPS');
-    --createInsertScript('PRJ_PPS_IBN');
-    createInsertScript('PRJ_SHUTTLE');
-    --createInsertScript('PRJ_SHUTTLE_TO');
-    --createInsertScript('PRJ_SIMU_CFG');
-    --createInsertScript('PRJ_SKID');
-    createInsertScript('PRJ_TERMINAL');
-    createInsertScript('SLM_CLONEIDMAP');
-    createInsertScript('SLM_EXECUTOR');
-    createInsertScript('SLM_EXECUTOR_CHAIN');
-    --createInsertScript('SLM_LOCATION'); -- generated by PL/SQL
-    createInsertScript('SLM_LOCATIONLOCK');
-    createInsertScript('SLM_LOCBLOCKING');
-    createInsertScript('SLM_LOCNEIGHBOUR');
-    createInsertScript('SLM_LOCPROPVAL');
-    createInsertScript('SLM_LOCRSC');
-    createInsertScript('SLM_LOCTYPE');
-    createInsertScript('SLM_LOCTYPEPROP');
-    createInsertScript('SLM_PATCHSET');
-    createInsertScript('SLM_PATCHSETHDR');
-    createInsertScript('SLM_PROPSET');
-    createInsertScript('SLM_PROPSET_PROP');
-    createInsertScript('SLM_SELNODE');
-    --createInsertScript('SLM_TEMP_LOCRANGELIST'); -- temporary table
-    --createInsertScript('SLM_TEMP_PROPVALTABLE'); -- temporary table
-    --createInsertScript('SLM_TEMP_PROPVALUES');   -- temporary table
-    createInsertScript('SLM_VALIDATOR');
-    createInsertScript('SLM_VALIDATOR_CHAIN');
-    createInsertScript('SLM_VALUESET');
-    createInsertScript('SLM_VALUESET_VALUE');
+    createInsertScript('departments');
+    createInsertScript('employees');
 
     DBMS_OUTPUT.PUT_LINE('END;');
     DBMS_OUTPUT.PUT_LINE('/');
 
 END;
-
 /
 
-
--- generate procedure call for all tables
-SELECT '    createInsertScript(''' || table_name || ''');'
-  FROM user_tables
- ORDER BY table_name
-;
-/
 

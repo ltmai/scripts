e6369165e3a6562b6c17e7960a8012c01950c059
mail <linh.tuan.mai@eisenmann.com>
2018-10-26 21:56:58 +0200

    Added license header

--- oralce/generate_insert_scripts/generateInsertScripts.cmd
+++ oralce/generate_insert_scripts/generateInsertScripts.cmd
@@ -1,19 +1,38 @@
-:: Wrapper to setup environment for running SQL script that write text with special characters (German)
 
-@echo off
+:: Generate INSERT script
+:: Copyright (C) 2018  - Linh Mai
+:: 
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+:: 
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+:: 
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-setlocal
+:: The following is an example wrapper script to setup environment for 
+:: running SQL script that write text with special characters (German)
 
-:: set console display codepage to Windows-1252 (single byte character encoding for Western European languages)
-chcp 1252
+@ECHO OFF
+
+SETLOCAL
+
+:: set console display codepage to Windows-1252 (single byte character 
+:: encoding for Western European languages)
+CHCP 1252
 
 :: set Oracle client encoding to the same codepage Windows-1252
-set NLS_LANG=.WE8MSWIN1252
+SET NLS_LANG=.WE8MSWIN1252
 
-sqlplus scott/tiger@ORCL @generateInsertScripts
+sqlplus scott/tiger@ORCL @generateInsertScripts.sql
 
 powershell -file TrimTrailingSpacesInFile.ps1 INSERTscript.LST INSERTscript.sql
 
-del INSERTscript.LST
+DEL INSERTscript.LST
 
-endlocal
+ENDLOCAL
--- oralce/generate_oracle_loader_files/generateLoaderScripts.cmd
+++ oralce/generate_oracle_loader_files/generateLoaderScripts.cmd
@@ -1,12 +1,29 @@
-:: generate loader scripts
+:: Generate Oracle LOADER script
+:: Copyright (C) 2018  - Linh Mai
+:: 
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+:: 
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+:: 
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-@echo off
+:: The following is an example wrapper script to setup environment for 
+:: running SQL script that write text with special characters (German)
 
-setlocal
+@ECHO OFF
 
-del output\*.dat
+SETLOCAL
 
-set dbconn=scott/tiger@ORCL
+DEL output\*.dat
+
+SET dbconn=scott/tiger@ORACLE
 
 :: change table name, add tables as 
 sqlplus %dbconn% @generateLoaderScript dept
@@ -15,11 +32,11 @@ sqlplus %dbconn% @generateLoaderScript bonus
 sqlplus %dbconn% @generateLoaderScript salgrade
 
 :: remove trailing spaces
-@for %%a in (output\*.LST) do ( 
+@FOR %%a IN (output\*.LST) DO ( 
     powershell -file TrimTrailingSpacesInFile.ps1 %%a output\%%~na.dat
 )
 
 :: remove .LST files
-del output\*.LST
+DEL output\*.LST
 
-endlocal
+ENDLOCAL
